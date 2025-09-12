f4454d998844a3a5d05f1fe70a915e62e4fbb360
shengshampoo <shengzh@tutanota.com>
2025-09-12 12:16:09 +0000

    Create build-static-crowdsec.sh

---
+++ build-static-crowdsec.sh
@@ -0,0 +1,48 @@
+
+#! /bin/bash
+
+set -e
+
+WORKSPACE=/tmp/workspace
+mkdir -p $WORKSPACE
+mkdir -p /work/artifact
+
+# abseil-cpp
+cd $WORKSPACE
+git clone https://github.com/abseil/abseil-cpp.git
+cd abseil-cpp
+mkdir build0
+cd build0
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=yes -DABSL_BUILD_TESTING=OFF -DABSL_USE_GOOGLETEST_HEAD=ON -DCMAKE_CXX_STANDARD=17 ..
+ninja
+ninja install
+cd ../
+mkdir build1
+cd ./build1
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=no -DABSL_BUILD_TESTING=OFF -DABSL_USE_GOOGLETEST_HEAD=ON -DCMAKE_CXX_STANDARD=17 ..
+ninja
+ninja install
+
+# re2 
+cd $WORKSPACE
+git clone https://github.com/google/re2
+cd re2
+sed -i '/^LDFLAGS = /s/ = / = -static --static -no-pie -s/' ./Makefile
+sed -i '/^prefix=/s/local//' ./Makefile
+make
+make install
+
+# crowdsec
+git clone https://github.com/crowdsecurity/crowdsec
+cd crowdsec
+CGO_CFLAGS="-D_LARGEFILE64_SOURCE" CGO_LDFLAGS="-static --static -no-pie -s -L/usr/lib" make BUILD_STATIC=1 release
+
+# cs-firewall-bouncer
+git clone https://github.com/crowdsecurity/cs-firewall-bouncer.git
+cd cs-firewall-bouncer
+make release
+
+cp /tmp/workspace/crowdsec/crowdsec-release.tgz /work/artifact
+cp /tmp/workspace/cs-firewall-bouncer/crowdsec-firewall-bouncer.tgz /work/artifact 
+
+
